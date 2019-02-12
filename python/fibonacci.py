c98d3a37c3133ac1c93b869797cdfcb59b35a470
wgawlikowska <weronika.gawlikowska@lo1.sandomierz.pl>
2019-02-12 13:30:53 +0100

    Ciąg Fibbonaciego

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -8,18 +8,38 @@
 #F(n)=F(n-1)+F(n-2) dla n>1
 
 def czy_naturalna(n):
-    if n isdigit():
+    if n isdigit ():
     
-        return int(a)
+        return True
     return False
 
+def fib_it(n):
+    
+    if n < 2: 
+        return n
+    a, b, wynik = 0, 1, 0
+    
+    for i in range(2, n+1):
+        if a >0:
+        print("{}/{} = {}".format(b,a,b/a))
+        wynik=a+b
+        a+b 
+        a, b=b,wynik
+    return wynik
+    
 
+        
+        
 def main(args):
+    assert fib_it(0) == 0
+    assert fib_it(3) == 10
     n = input('Który wyraz ciągu')
-    while not pobierz_liczbe(n):
+    while not czy naturalna(n):
         print('Błędne dane!')
     n = input('Który wyraz ciągu?')
     
+    print("F_it({})={})".format(n, fib_it(int(n))))
+    fib_it(i)
     
     return 0
 
