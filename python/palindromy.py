0c00b764fc75d85948ffe2a26c1c07683cc109a5
wgawlikowska <weronika.gawlikowska@lo1.sandomierz.pl>
2019-03-04 12:35:43 +0100

    Palindromy

---
+++ python/palindromy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  palindromy.py
+
+import os
+
+def czy_palindrom(tekst):
+    ile = len(tekst)
+    for i in range(ile//2):
+        if tekst[i]!=tekst[-i-1]:
+            return False
+    return True
+    
+def czytaj_dane(plik):
+    if not os.path_exists(plik):
+        print('Plik niedostepny!')
+        return False
+    teksty=[]
+    with open(plik,"r") as f:
+        for wiersz in f:
+            teksty.append(linia.strip())
+    return teksty
+def main(args):
+    #tekst=input('Podaj tekst')
+    teksty = czytaj_dane('dane01.txt')
+    print(teksty)
+    ile = 0
+    for
+    # ~if czy_palindrom(tekst):
+        # ~print('To Palindrom')
+            # ~else:
+        # ~print('To nie palindrom')
+
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
